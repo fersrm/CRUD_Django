4862fedf4610cf1ef5d4e56ffc084c0c9d3adf89
Fernando <fer.rosasmardones@gmail.com>
2023-04-23 20:11:55 -0400

    se separo funciones de js

--- ProyecBazarApp/static/ProyecBazarApp/js/script.js
+++ ProyecBazarApp/static/ProyecBazarApp/js/script.js
@@ -6,26 +6,3 @@ sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
   hambur.classList.toggle("activo");
 });
-
-
-
-//----------------------MODAL--------------------------------------
-let modal = document.getElementById("agregar-producto");
-let btn = document.getElementById("abrir-modal");
-let span = document.getElementById("cerrar-modal");
-// Cuando el usuario hace clic en el botón, mostrar la ventana modal
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
-// Cuando el usuario hace clic en el botón de cerrar, ocultar la ventana modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// Cuando el usuario hace clic fuera de la ventana modal, ocultarla
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-//-----------Mensajes-------------------
