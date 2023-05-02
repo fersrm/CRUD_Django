10215db42a77587b7e5949e92b6a4f8808c4f51b
Fernando <fer.rosasmardones@gmail.com>
2023-05-01 20:51:57 -0400

    img prueba 4

--- build.sh
+++ build.sh
@@ -5,11 +5,8 @@ set -o errexit
 #poetry install
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
 
 # Definir la variable de entorno para la URL de medios
 export MEDIA_URL='/media/'
-
-# Copiar los archivos de medios en la carpeta MEDIA_ROOT
-python manage.py collectmedia --no-input
