19298e90c301581e41d758ddb0f32333a051bc12
Fernando <fer.rosasmardones@gmail.com>
2023-05-02 14:48:36 -0400

    responsibo produccion

--- ProyecBazarApp/static/ProyecBazarApp/js/script.js
+++ ProyecBazarApp/static/ProyecBazarApp/js/script.js
@@ -1,8 +1,14 @@
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
 let hambur= document.getElementById("MenuHambur");
+
+let contenido = document.getElementById("contenido");
+let encabezado = document.getElementById("encabezado");
+
 //console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
   hambur.classList.toggle("activo");
+  contenido.classList.toggle("none");
+  encabezado.classList.toggle("none");
 });
