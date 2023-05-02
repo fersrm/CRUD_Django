3f2c8d29547f45e9f51160ce961d1093ba8289ed
Fernando <fer.rosasmardones@gmail.com>
2023-05-01 20:46:45 -0400

    imagen media prueba 3

--- build.sh
+++ build.sh
@@ -7,3 +7,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Definir la variable de entorno para la URL de medios
+export MEDIA_URL='/media/'
+
+# Copiar los archivos de medios en la carpeta MEDIA_ROOT
+python manage.py collectmedia --no-input
