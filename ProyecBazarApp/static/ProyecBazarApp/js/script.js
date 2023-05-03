1a03dbc933238174bacc8fc758a62e34689a3f16
Fernando <fer.rosasmardones@gmail.com>
2023-05-03 12:29:09 -0400

    cambios menores 2

--- ProyecBazarApp/static/ProyecBazarApp/js/script.js
+++ ProyecBazarApp/static/ProyecBazarApp/js/script.js
@@ -3,10 +3,12 @@ let sidebarBtn = document.querySelector(".bx-menu");
 let hambur= document.getElementById("MenuHambur");
 
 let contenido = document.getElementById("body");
+let encabezado = document.getElementById("encabezado");
 
 //console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
   hambur.classList.toggle("activo");
   contenido.classList.toggle("none");
+  encabezado.classList.toggle("none");
 });
